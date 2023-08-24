a4513e41523c07e2d014f5d002caee549bde8cad
Fajar Nugraha <88820254+fajarnu@users.noreply.github.com>
2023-08-24 12:29:18 +0700

    Update tombol.py

--- Tombol/tombol.py
+++ Tombol/tombol.py
@@ -1,66 +1,31 @@
 from flask import Flask, render_template, request
-import requests
-import time
+from ubidots import ApiClient
 
 app = Flask(__name__)
 
-# Masukkan token dan ID Variable dari Ubidots di bawah ini
-TOKEN = "BBFF-thUhhRPJojoHiUB78bozuZuPy2dKTv"
-DEVICE_LABEL = "farmops"
-VARIABLE_LABEL_1 = "test-tombol"
+# Ubah dengan token dan label yang sesuai dari Ubidots
+API_TOKEN = "BBFF-thUhhRPJojoHiUB78bozuZuPy2dKTv"
+LABEL = "64cb734bdfc2f3000b9aec5b"
 
-# Status default ketika aplikasi pertama kali dijalankan
-status = 0
+api = ApiClient(token=API_TOKEN)
+variable = api.get_variable(LABEL)
+
+def toggle_value(current_value):
+    if current_value == 0:
+        return 1
+    else:
+        return 0
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global status
     if request.method == 'POST':
-        status = 1 - status  # Toggle status antara 0 dan 1
+        current_value = variable.get_values(1)[0]['value']
+        new_value = toggle_value(current_value)
+        variable.save_value({'value': new_value})
 
-    return render_template('index.html', status=status)
-
-def build_payload(variable_1):
-
-    tombol = 1
-    
-    payload = {
-        variable_1: tombol
-    }
-    return payload
-
-def post_request(payload):
-    # Creates the headers for the HTTP requests
-    url = "http://industrial.api.ubidots.com"
-    url = "{}/api/v1.6/devices/{}".format(url, DEVICE_LABEL)
-    headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
-
-    # Makes the HTTP requests
-    status = 400
-    attempts = 0
-    while status >= 400 and attempts <= 5:
-        req = requests.post(url=url, headers=headers, json=payload)
-        status = req.status_code
-        attempts += 1
-        time.sleep(1)
-
-    # Processes results
-    if status >= 400:
-        print("[ERROR] Could not send data after 5 attempts, please check \
-            your token credentials and internet connection")
-        return False
-
-    print("[INFO] request made properly, your device is updated")
-    return True
-
-
-def main():
-    payload = build_payload(
-        VARIABLE_LABEL_1)
-    print(payload)
-    print("[INFO] Attemping to send data")
-    post_request(payload)
-    print("[INFO] finished")
+    current_value = variable.get_values(1)[0]['value']
+    return render_template('index.html', current_value=current_value)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
